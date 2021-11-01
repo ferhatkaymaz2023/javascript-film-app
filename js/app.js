5eeedba7d1738e1587eb76b5a17fc7d3060a77c1
ferhatkaymaz2023 <ferhatkaymaz2023@gmail.com>
2021-11-01 15:38:16 +0300

    Dosya iskeleti oluşturuldu !

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('film-form');
+const firstCardBody = document.querySelectorAll('.card-body')[0];
+const titleElement = document.getElementById('title');
+const directorElement = document.getElementById('director');
+const urlElement = document.getElementById('url');
+
+
+addEventListeners();
+
+function addEventListeners()
+{
+    
+}
